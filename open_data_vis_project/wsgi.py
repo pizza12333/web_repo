0c7a3e8619e83134e859d69542f8cb1ff0218fa1
pizza12333 <pizza12333@naver.com>
2017-07-06 14:38:37 +0900

    wsgi file setting

--- open_data_vis_project/wsgi.py
+++ open_data_vis_project/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/JEONGWOOJIN/web_repo/'  # PythonAnywhere 계정으로 바꾸세요.
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'open_data_vis_project.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "open_data_vis_project.settings")
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
